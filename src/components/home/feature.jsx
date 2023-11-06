3553d1b4113e178b5a2673f8460bad9d63f9bda3
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-11-06 20:01:29 +0530

    improved feature section

--- src/components/home/feature.jsx
+++ src/components/home/feature.jsx
@@ -1,15 +1,17 @@
 import schedule from '@/assets/schedule.svg';
 import appointment from '@/assets/appointment.svg';
 import support from '@/assets/support.svg';
+import recommendation from '@/assets/recommendation.svg';
 import Image from 'next/image';
 
 const Feature = () => {
   return (
-    <section className='w-full bg-highlight my-12 py-4' id='chat'>
-    <div className='flex flex-row  md:space-x-96 justify-center'>
-      <div className='flex flex-col  justify-center gap-1'><Image src={schedule} className='mx-auto w-max md:w-full'  width={128} height={128} alt='schedule'/> <h3 className='text-center'> Rapid Scheduling </h3> </div>
-      <div className='flex flex-col justify-center gap-1'><Image src={appointment} className='mx-auto w-max md:w-full'  width={128} height={128} alt='appointment'/> <h3 className='text-center'> Live Appointments</h3> </div>
-      <div className='flex flex-col justify-center gap-1'><Image src={support} className='mx-auto w-max md:w-full' width={128} height={128} alt='support'/> <h3 className='text-center'> 24/7 Support </h3> </div>
+    <section className='w-full bg-highlight my-12 py-12 md:py-4' id='chat'>
+    <div className='flex flex-col md:flex-row space-y-12 md:space-y-0 md:space-x-80 mx-24'>
+      <div className='grid grid-cols-1 place-items-center  '><div className='w-16 flex'><Image src={schedule} className='mx-auto w-max md:w-full'  width={75} height={75} alt='schedule'/> </div><h3 className='text-center'> Rapid Scheduling </h3> </div>
+      <div className='grid grid-cols-1 place-items-center'><div className='w-16 flex'><Image src={appointment} className='mx-auto w-max md:w-full'  width={75} height={75} alt='appointment'/> </div><h3 className='text-center'> Live Appointments</h3> </div>
+      <div className='grid grid-cols-1 place-items-center  '><div className='w-16 flex'><Image src={recommendation} className='mx-auto w-max md:w-full'  width={75} height={75} alt='appointment'/> </div><h3 className='text-center'> Recommendation System</h3> </div>
+      <div className='grid grid-cols-1 place-items-center  '><div className='w-16 flex'><Image src={support} className='mx-auto w-max md:w-full' width={75} height={75} alt='support'/></div> <h3 className='text-center'> 24/7 Support </h3> </div>
 
     </div>
     </section>
